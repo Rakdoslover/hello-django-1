ad22abb22eb79e8648ca1ab7d4aa97f5861c0078
Rakdoslover <>
2023-06-07 11:04:58 +0000

    added some of the items stats to the basic html

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a++2^pcmtbzm%=6lfb@8^9ss6ej_bexc4q@6usr4sh70oz14sq'
+SECRET_KEY = 'django-insecure-a++2^pcmtbzm%=6lfb@8^9ss6ej_bexc4q@', \
+    '6usr4sh70oz14sq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
